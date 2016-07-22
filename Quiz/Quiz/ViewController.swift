a56cfd46041041b9ee1cfbe7c8deb845f9e8c502
Marcin Sporysz <sporysz@techsterowniki.pl>
2016-07-22 14:34:39 +0200

    Basic UI

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -13,6 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
 
+    let questions: [String] = ["Fromwhat is cognac mage?",
+                               "Whats is 7+7?",
+                               "What is the capital of Vermont?"]
+    
+    let answers: [String] = ["Grapes",
+                            "14",
+                            "Montpelier"]
 
+    
+    var currentQuestionIndex: Int = 0
+    
+    
+    
+    @IBAction func showNextQuestion(sender: AnyObject)
+    {
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject)
+    {
+    
+    }
+    
 }
 
