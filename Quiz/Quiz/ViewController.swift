aaa91d09efaf898a94894330a9f088893cdd3159
Marcin Sporysz <sporysz@techsterowniki.pl>
2016-07-22 10:33:45 +0200

    Start Quiz app

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Marcin Sporysz on 22.07.2016.
+//  Copyright © 2016 Marcin Sporysz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+
+
+}
+
