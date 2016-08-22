0f14c751937e9edc7d98d7d63e723d3ecc2d38d7
Marcin Sporysz <sporysz@techsterowniki.pl>
2016-08-22 14:08:40 +0200

    Aplikacja do nauki działania kontroli NSOutlineView - na potrzbeby aplikacji CoreBluetoothMacOS.

---
+++ OutlineViewTests/OutlineViewTests/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OutlineViewTests
+//
+//  Created by Marcin Sporysz on 22/08/16.
+//  Copyright © 2016 Marcin Sporysz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OutlineViewTests/OutlineViewTests/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OutlineViewTests
+//
+//  Created by Marcin Sporysz on 22/08/16.
+//  Copyright © 2016 Marcin Sporysz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
